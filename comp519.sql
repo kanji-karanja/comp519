72e278dd877c565f8416b70f49e106af144bd925
kanji-karanja <karimkanji101@gmail.com>
2019-02-19 07:12:30 +0300

    Updated the new database with the admin table added to sql.

--- comp519.sql
+++ comp519.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.2
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 08, 2019 at 08:18 AM
--- Server version: 10.1.34-MariaDB
--- PHP Version: 7.2.7
+-- Generation Time: Feb 19, 2019 at 05:09 AM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.3.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -27,6 +27,25 @@ USE `comp519`;
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` int(11) NOT NULL,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`id`, `username`, `password`) VALUES
+(1, 'kanji-karanja', '9996535e07258a7bbfd8b132435c5962');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `slots`
 --
 
@@ -42,8 +61,8 @@ CREATE TABLE `slots` (
 --
 
 INSERT INTO `slots` (`id`, `day`, `time`, `count`) VALUES
-(1, 'Monday', '15.00-17.00', 0),
-(2, 'Tuesday', '14.00-16.00', 0),
+(1, 'Monday', '15.00-17.00', 8),
+(2, 'Tuesday', '14.00-16.00', 8),
 (3, 'Thursday', '11.00-13.00', 0),
 (4, 'Friday', '10.00-12.00', 0);
 
@@ -63,8 +82,36 @@ CREATE TABLE `student_course` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Dumping data for table `student_course`
+--
+
+INSERT INTO `student_course` (`id`, `name`, `firstname`, `sid`, `email`, `slot_id`) VALUES
+(1, 'Karim Kanji', 'Emmanuel', '1111', 'karimkanji101@gmail.com', 1),
+(2, 'Karim Kanji', 'Emmanuel', '11117', 'karimkanji101@gmail.com', 1),
+(3, 'Karim Kanji', 'Emmanuel', '111175', 'karimkanji101@gmail.com', 1),
+(4, 'Karim Kanji', 'Emmanuel', '1111757', 'karimkanji101@gmail.com', 1),
+(5, 'Karim Kanji', 'Emmanuel', '11117578', 'karimkanji101@gmail.com', 1),
+(6, 'Karim Kanji', 'Emmanuel', '111175780', 'karimkanji101@gmail.com', 1),
+(7, 'Karim Kanji', 'Emmanuel', '1111757806', 'karimkanji101@gmail.com', 1),
+(8, 'Karim Kanji', 'Emmanuel', '11117578066', 'karimkanji101@gmail.com', 1),
+(9, 'Karim Kanji', 'Emmanuel', '2', 'karimkanji101@gmail.com', 2),
+(10, 'Karim Kanji', 'Emmanuel', '23', 'karimkanji101@gmail.com', 2),
+(11, 'Karim Kanji', 'Emmanuel', '236', 'karimkanji101@gmail.com', 2),
+(12, 'Karim Kanji', 'Emmanuel', '2368', 'karimkanji101@gmail.com', 2),
+(13, 'Karim Kanji', 'Emmanuel', '23680', 'karimkanji101@gmail.com', 2),
+(14, 'Karim Kanji', 'Emmanuel', '236805', 'karimkanji101@gmail.com', 2),
+(15, 'Karim Kanji', 'Emmanuel', '23680529', 'karimkanji101@gmail.com', 2),
+(16, 'Karim Kanji', 'Emmanuel', '236805290', 'karimkanji101@gmail.com', 2);
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `slots`
@@ -83,6 +130,12 @@ ALTER TABLE `student_course`
 --
 
 --
+-- AUTO_INCREMENT for table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `slots`
 --
 ALTER TABLE `slots`
@@ -92,7 +145,7 @@ ALTER TABLE `slots`
 -- AUTO_INCREMENT for table `student_course`
 --
 ALTER TABLE `student_course`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
